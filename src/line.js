a8aec79009a9bb508a5fa2ecd4026f14974ef960
Ruby <12829001+rubyzhao@users.noreply.github.com>
2019-11-21 23:04:49 +0300

    Add files via upload

---
+++ src/line.js
@@ -0,0 +1,34 @@
+<!--Actual chart related script tag-->
+
+// Replace the contents of this script tag if you want to test code from our examples:
+// https://www.arction.com/lightningchart-js-interactive-examples/
+
+// Extract required parts from LightningChartJS.
+const {
+    lightningChart
+} = lcjs //Note: @arction/lcjs is not needed here, when using IIFE assembly
+
+// Create a XY Chart.
+const chart = lightningChart().ChartXY({
+    // Set the chart into a div with id, 'target'. 
+    // Chart's size will automatically adjust to div's size. 
+    containerId: 'target'
+})
+    .setTitle('Sine Wave') // Set chart title
+
+    var chartData = [];
+    var loop = 2e4;
+    //2e5: ok in Chorme, but pwmetrics:Unable to complete run 1 of 5 due to No metrics data
+    
+    for (var i = 0; i < loop; i++) {
+        //loop=5e6; will get Aw, Snap error for the following:
+        
+        var x = (800 * Math.PI * i) / loop;
+        var y = 100 * Math.sin(i);
+        chartData.push({ x: x, y: y });
+    }
+
+// Add a line series.
+const lineSeries = chart.addLineSeries()
+    .setName('My data')
+    .add(chartData)
